52d5f7355b83c9e70428d1f4ce4b906e558b2643
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-10-05 01:59:37 +0900

    `Vite` が `@/*` を読み込むように `vite.config.ts` を変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     react(),
     tsconfigPaths(),

66cf7221d23a4cace7ac4959c2e3520856eba3ab
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-10-25 23:35:18 +0900

    Javaのオブジェクトと同様に `CalendarCell` の `class` を `classAt` に変更.

--- frontend/src/types/dataType.tsx
+++ frontend/src/types/dataType.tsx
@@ -4,24 +4,24 @@ export type ClassName =
 
 export type CalendarCell = {
     id: number,
-    class: ClassName,
+    classAt: Date;
     has_classes: Class[],
-}
+};
 
 export type Class = {
     id: number,
     name: ClassName,
     classDetails: ClassDetail[],
-}
+};
 
 export type ClassDetail = {
     id: number,
-    student: string,
-    subject: string,
-    memo: string,
-    unit: string,
-    leaned: string,
-    goodPoint: string,
-    issue: string,
-    comment: string,
-}
+    student?: string,
+    subject?: string,
+    memo?: string,
+    unit?: string,
+    leaned?: string,
+    goodPoint?: string,
+    issue?: string,
+    comment?: string,
+};

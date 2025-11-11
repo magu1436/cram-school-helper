b74c4ebb337c01321372614f03379374e4bb305b
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-11-11 20:33:15 +0900

    `ClassMapper` の `getClassesByDate` の `calendar_cell_id` が `calendarcell_id` になっていたので修正. `ClassController` のCrossOriginのURLが間違っていたので修正

--- backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassController.java
+++ backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@CrossOrigin(origins = "http://localhost:5137")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/class")

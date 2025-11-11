d3506d01c1dfa49e98798237895c4618e8c9e948
magu1436 <hfzoro77@gmail.com>
2025-11-11 17:35:57 +0900

    `getClassesByDate` が `Class[]` ではなく `ClassApiObj[]` を返してしまっていたのを修正

--- frontend/src/features/classDetails/api/api.tsx
+++ frontend/src/features/classDetails/api/api.tsx
@@ -1,7 +1,9 @@
-import type { Class } from "@/types/dataType";
+import { toClass } from "@/api/mapper";
+import type { Class, ClassApiObj } from "@/types/dataType";
 import type { fetchReturnType } from "@/types/fetchType";
 import { formatDate } from "@/utils/dateformatter";
 import useFetch from "@/utils/fetch";
+import { useEffect, useState } from "react";
 
 export const createComment = (
     subject: string,
@@ -17,11 +19,20 @@ export const createComment = (
 export const getClassesByDate = (date: Date): fetchReturnType<Class[]> => {
     const apiUrl = "/classes/getClassesByDate";
 
-    return useFetch<Class[]>(
+    const {data, isLoading, error} = useFetch<ClassApiObj[]>(
         apiUrl,
         "GET",
         {
             date: formatDate(date, "YYYY-MM-DD")
         },
     );
+
+    const [ classes, setClasses ] = useState<Class[]>();
+
+    useEffect(() => {
+        if (!data) return;
+        setClasses(data.map(d => toClass(d)));
+    }, [data]);
+
+    return { data: classes, isLoading, error }
 };

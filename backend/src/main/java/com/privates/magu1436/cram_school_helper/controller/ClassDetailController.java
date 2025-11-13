35f498cd76e6f8bea0e473194fe4db0ef23f4d56
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-11-13 20:41:03 +0900

    DBで自動生成されたidをオブジェクトに入れ, その値をフロントに返却する仕様に変更

--- backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
+++ backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,8 @@ public class ClassDetailController {
     private final ClassDetailMapper mapper;
 
     @PostMapping("/create")
-    public ResponseEntity<Integer> createClassDetail(CreateClassDetailForm form){
-        Integer id = mapper.insertClassDetail(form);
-        return new ResponseEntity<>(id, HttpStatus.CREATED);
+    public ResponseEntity<Integer> createClassDetail(@RequestBody CreateClassDetailForm form){
+        mapper.insertClassDetail(form);
+        return new ResponseEntity<>(form.getClassDetail().getId(), HttpStatus.CREATED);
     }
 }

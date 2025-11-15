95d12076ac44d7638224963a49b534d1c120b27f
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-11-15 15:09:28 +0900

    バックエンド側で `deleteClassDetail` APIを実装

--- backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
+++ backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
@@ -3,6 +3,8 @@ package com.privates.magu1436.cram_school_helper.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,11 @@ public class ClassDetailController {
     public ResponseEntity<Integer> createClassDetail(@RequestBody CreateClassDetailForm form){
         mapper.insertClassDetail(form);
         return new ResponseEntity<>(form.getClassDetail().getId(), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteClassDetail(@PathVariable int id){
+        mapper.deleteClassDetail(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }

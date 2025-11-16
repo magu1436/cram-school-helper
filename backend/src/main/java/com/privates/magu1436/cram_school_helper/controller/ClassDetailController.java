c859a4680290cfaf8933d9d3526dbf8680dc821e
magu1436 <hfzoro77@gmail.com>
2025-11-16 18:29:23 +0900

    `updateClassDetail` APIコントローラーの実装.

--- backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
+++ backend/src/main/java/com/privates/magu1436/cram_school_helper/controller/ClassDetailController.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.privates.magu1436.cram_school_helper.entity.ClassDetail;
 import com.privates.magu1436.cram_school_helper.form.CreateClassDetailForm;
+import com.privates.magu1436.cram_school_helper.form.UpdateClassDetailForm;
 import com.privates.magu1436.cram_school_helper.mapper.ClassDetailMapper;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -33,5 +37,11 @@ public class ClassDetailController {
     public ResponseEntity<Void> deleteClassDetail(@PathVariable int id){
         mapper.deleteClassDetail(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<Void> updateClassDetail(@RequestBody UpdateClassDetailForm form){
+        mapper.updateClassDetail(form);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
